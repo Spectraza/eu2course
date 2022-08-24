18452449394f879a51f44eda6dffde11e8863990
zealot <savichpg@yahoo.com>
2022-08-24 14:13:16 +0300

    init

--- JavaQuests/src/Main.java
+++ JavaQuests/src/Main.java
@@ -3,13 +3,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner keyboard = new Scanner(System.in);
 
-        double a = keyboard.nextDouble();
-        double b = keyboard.nextDouble();
+//          Quest #1
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        double a = keyboard.nextDouble();
+//        double b = keyboard.nextDouble();
+//
+//        double s = Math.pow(a, 2) + b * b;
+//        System.out.println("Hypotenuse is " + s);
 
-        double s = Math.pow(a, 2) + b * b;
-        System.out.println("Hypotenuse is " + s);
+
+//          Quest #2
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        int a = keyboard.nextInt();
+//
+//        int b = a + 1;
+//
+//        int c = a - 1;
+//
+//        System.out.println("The next number " + a + " is " + b);
+//        System.out.println("The previous number " + a + " is " + c);
 
 
     }
