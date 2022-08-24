e95dcec226cfd704698ac0388b87366850c1a53e
zealot <savichpg@yahoo.com>
2022-08-24 13:58:23 +0300

    init

--- JavaQuests/src/Main.java
+++ JavaQuests/src/Main.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner keyboard = new Scanner(System.in);
+
+        double a = keyboard.nextDouble();
+        double b = keyboard.nextDouble();
+
+        double s = Math.pow(a, 2) + b * b;
+        System.out.println("Hypotenuse is " + s);
+
 
     }
 }
