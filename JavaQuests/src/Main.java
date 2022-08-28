14b94ef8cae5f77ef6e4d5c9d8d86f3e07718d78
Spectraza <96127639+Spectraza@users.noreply.github.com>
2022-08-28 16:18:16 +0200

    Practise1 (#2)
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    Co-authored-by: zealot <savichpg@yahoo.com>

---
+++ JavaQuests/src/Main.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+
+public class Main {
+
+//    For 3 and 4 quests
+//    private static void docs() {
+//        System.out.println("Invalid input\n" +
+//                "\n" +
+//                "This program calculates apples which each student will get. " +
+//                "First enter number of students and then number of apples!");
+//    }
+//
+//    private static int calcApples(int n, int k) {
+//        return (k / n);
+//    }
+//
+//    private static int calcRemaining(int n, int k) {
+//        return (k % n);
+//    }
+
+    public static void main(String[] args) {
+
+
+//          Quest #1
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        double a = keyboard.nextDouble();
+//        double b = keyboard.nextDouble();
+//
+//        double s = Math.pow(a, 2) + b * b;
+//        System.out.println("Hypotenuse is " + s);
+
+
+//          Quest #2
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        int a = keyboard.nextInt();
+//
+//        int b = a + 1;
+//
+//        int c = a - 1;
+//
+//        System.out.println("The next number " + a + " is " + b);
+//        System.out.println("The previous number " + a + " is " + c);
+
+
+//           Quests #3 and 4
+//        int n = -1;
+//        int k = -1;
+//
+//        try {
+//            n = Integer.parseInt(args[0]);
+//            k = Integer.parseInt(args[1]);
+//        } catch (Exception e) {
+//            docs();
+//            return;
+//        }
+//
+//
+//        if (args.length != 2) {
+//            docs();
+//            return;
+//
+//        }
+//        int resultthree = calcApples(n, k);
+//        System.out.println(resultthree);
+//
+//        int resultfour = calcRemaining(n, k);
+//        System.out.println(resultfour);
+//
+//
+    }
+
+
+}
