22f2a8baffb2616bd357965927453c73ebc6d49d
Spectraza <savychvp@gmail.com>
2023-03-02 12:52:47 +0200

    added tabs

--- slider/src/app/app.component.ts
+++ slider/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'slider';
+  currentPage = 0;
+  pages = [
+    {name: "tab 1", content: "lorem"},
+    {name: "tab 2", content: "lorem"},
+    {name: "tab 3", content: "lorem"},
+    {name: "tab 4", content: "lorem"}
+  ];
+
+  test(){
+    console.log(this.currentPage)
+  }
 }
--- slider/src/app/app.module.ts
+++ slider/src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {MatSliderModule} from "@angular/material/slider";
+import {FormsModule} from "@angular/forms";
+import {MatTabsModule} from "@angular/material/tabs";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        MatSliderModule
-    ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    FormsModule,
+    MatTabsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
