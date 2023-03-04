7f5eb45117a03e202ab1dc634bd5c96e5ee6b2e1
Spectraza <96127639+Spectraza@users.noreply.github.com>
2023-03-04 19:24:18 +0200

    added comments here too (#6)

--- slider/src/app/app.component.ts
+++ slider/src/app/app.component.ts
@@ -7,12 +7,12 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'slider';
-  currentPage = 0;
+  currentPage = 0; // variable to connect slider and tabs
   pages = [
     {name: "tab 1", content: "lorem"},
     {name: "tab 2", content: "lorem"},
     {name: "tab 3", content: "lorem"},
-    {name: "tab 4", content: "lorem"}
+    {name: "tab 4", content: "lorem"} //massive of pages
   ];
 
   test(){
