423ff6fc162f5e8f7af62558269eb3dd7adb1c05
Spectraza <savychvp@gmail.com>
2023-05-04 23:39:26 +0300

    first, second are done

---
+++ JavaQuests/first-themes-tasks/Tasks-From-First-Themes/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //        Дано пятизначное натуральное число. Определить является, ли это число палиндромом.
+        System.out.println("Task 1");
+
+        int number = 33333;
+        int n = 0;
+        int utNum = number;
+
+        while (utNum != 0) {
+            n++;
+            utNum /= 10;
+        }
+
+        boolean isPolindrome = true;
+
+        for (int i = 0; i <= n; i++) {
+            int a = (int) (number / Math.pow(10, i)) % 10;
+            int b = (int) (number / Math.pow(10, n - 1 - i)) % 10;
+
+            if (a != b) {
+                isPolindrome = false;
+                break;
+            }
+
+        }
+
+        if (isPolindrome) {
+            System.out.println("Polimdrome");
+        } else {
+            System.out.println("Not Polindrome");
+        }
+    }
+}
