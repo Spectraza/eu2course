9721e9822303270feea8d11767149ea57c1634c3
Spectraza <savychvp@gmail.com>
2023-05-12 05:41:24 +0300

    task eight is done

--- JavaQuests/massives/task-massives/src/TaskSeven.java
+++ JavaQuests/massives/task-massives/src/TaskSeven.java
@@ -2,12 +2,12 @@
 // Найдите наименьший четный элемент массива. Если такого нет, то выведите первый элемент.
 
 public class TaskSeven {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] nums = new int[]{4, 5, 6, 2, 3};
         int min = nums[0];
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0 && nums[i] < min){
+            if (nums[i] % 2 == 0 && nums[i] < min) {
                 min = nums[i];
             }
         }
