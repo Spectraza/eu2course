49cfb55d4f7568119065dfe4117ae6fbce109838
Spectraza <savychvp@gmail.com>
2023-05-12 05:23:45 +0300

    task six is done

---
+++ JavaQuests/massives/task-massives/src/TaskSix.java
@@ -0,0 +1,20 @@
+
+
+// Найдите наибольший элемент массива.
+
+public class TaskSix {
+    public static void main(String[] args) {
+
+        int[] nums = new int[]{4, 5, 6, 2, 3};
+        int max = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > max) {
+                max = nums[i];
+            }
+        }
+
+        System.out.println(max);
+
+    }
+}
