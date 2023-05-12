3482602899ba3b175b716a55e13ae73c4e5ab955
Spectraza <savychvp@gmail.com>
2023-05-12 04:54:13 +0300

    just spaces

--- JavaQuests/massives/task-massives/src/TaskThree.java
+++ JavaQuests/massives/task-massives/src/TaskThree.java
@@ -2,11 +2,12 @@
 //  Найдите сумму отрицательных элементов массива.
 
 public class TaskThree {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] nums = new int[]{-3, -4, 5, 6, 3};
         int sum = 0;
+
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] < 0){
+            if (nums[i] < 0) {
                 sum += nums[i];
             }
         }
