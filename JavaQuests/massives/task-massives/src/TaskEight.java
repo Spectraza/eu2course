805ef21feb3d80f833993052815eda526d08a45b
Spectraza <savychvp@gmail.com>
2023-05-12 05:46:43 +0300

    task nine is done

--- JavaQuests/massives/task-massives/src/TaskEight.java
+++ JavaQuests/massives/task-massives/src/TaskEight.java
@@ -3,12 +3,12 @@ import java.util.Arrays;
 // Преобразовать массив так, чтобы сначала шли нулевые элементы, а затем все остальные.
 
 public class TaskEight {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] nums = new int[]{0, 1, 5, 0, 10, 0, 6, 0, -1};
         int zeroEnd = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != 0){
+            if (nums[i] != 0) {
                 continue;
             }
 
