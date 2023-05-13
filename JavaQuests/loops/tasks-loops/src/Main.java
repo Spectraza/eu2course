e23dd2be8f2e8c2231292db0aefb5a4986fb88bb
Spectraza <savychvp@gmail.com>
2023-05-13 22:53:53 +0300

    firs task has been complited

---
+++ JavaQuests/loops/tasks-loops/src/Main.java
@@ -0,0 +1,11 @@
+
+// Необходимо вывести на экран числа от 1 до 5. На экране должно быть: 1 2 3 4 5
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
