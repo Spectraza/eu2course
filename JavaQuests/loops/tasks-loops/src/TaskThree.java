9a6f45f327304d09d63a2b26b513e6b1c23afa18
Spectraza <savychvp@gmail.com>
2023-05-13 23:13:50 +0300

    third task has been complited

---
+++ JavaQuests/loops/tasks-loops/src/TaskThree.java
@@ -0,0 +1,11 @@
+public class TaskThree {
+    public static void main(String[] args){
+
+        int num = 3;
+        int mult;
+        for (int i = 1; i <= 10; i++) {
+            mult = num * i;
+            System.out.println(num + " * " + i + " = " + mult);
+        }
+    }
+}
