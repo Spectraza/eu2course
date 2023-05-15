94b7fb5ff365277e8a47ef7501a44f610b794552
Spectraza <savychvp@gmail.com>
2023-05-16 02:52:40 +0300

    changed a bit

--- JavaQuests/loops/tasks-loops/src/TaskFive.java
+++ JavaQuests/loops/tasks-loops/src/TaskFive.java
@@ -1,3 +1,6 @@
+
+// Необходимо, чтоб программа выводила на экран вот такую последовательность: 7 14 21 28 35 42 49 56 63 70 77 84 91 98
+
 public class TaskFive {
     public static void main(String[] args){
         int num = 7;
--- JavaQuests/loops/tasks-loops/src/TaskSix.java
+++ JavaQuests/loops/tasks-loops/src/TaskSix.java
@@ -1,6 +1,9 @@
+
+// Необходимо вывести на консоль такую последовательность чисел: 1 2 4 8 16 32 64 128 256 512
+
 public class TaskSix {
-    public static void main(String[] args){
-        for (int i = 1; i <= 64; i*= 2) {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 512; i *= 2) {
             System.out.println(i);
         }
     }
