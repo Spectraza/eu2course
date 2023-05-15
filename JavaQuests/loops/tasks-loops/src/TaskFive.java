42b3d128999df9d03c405fdeaa52d07382775759
Spectraza <savychvp@gmail.com>
2023-05-16 02:50:12 +0300

    task five has been complited

---
+++ JavaQuests/loops/tasks-loops/src/TaskFive.java
@@ -0,0 +1,11 @@
+public class TaskFive {
+    public static void main(String[] args){
+        int num = 7;
+        int mult;
+        for (int i = 1; i <=14 ; i++) {
+            mult = num * i;
+            System.out.println(mult);
+        }
+
+    }
+}
---
+++ JavaQuests/loops/tasks-loops/src/TaskSix.java
@@ -0,0 +1,7 @@
+public class TaskSix {
+    public static void main(String[] args){
+        for (int i = 1; i <= 64; i*= 2) {
+            System.out.println(i);
+        }
+    }
+}
