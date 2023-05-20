ee22a16e7c459605fde8a59d00f7a6598151830f
Spectraza <savychvp@gmail.com>
2023-05-20 22:58:31 +0300

    task four has been complited and changed a bit

--- JavaQuests/loops/task-two-loops/src/TaskThree.java
+++ JavaQuests/loops/task-two-loops/src/TaskThree.java
@@ -8,15 +8,12 @@
 // всего таких ошибочных табличек оказалось в бракованной партии.
 
 public class TaskThree {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         int totalDefective = 0;
 
-        for (int i = 1; i <= 50000; i++)
-        {
+        for (int i = 1; i <= 50000; i++) {
             String number = String.valueOf(i);
-            if (number.contains("2"))
-            {
+            if (number.contains("2")) {
                 totalDefective++;
             }
         }
