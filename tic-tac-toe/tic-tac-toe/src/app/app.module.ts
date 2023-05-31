356e09af73e7875a88abb4a37cceca6f3933fcfa
Spectraza <savychvp@gmail.com>
2023-05-31 23:48:31 +0300

    changed a bit

--- tic-tac-toe/tic-tac-toe/src/app/app.module.ts
+++ tic-tac-toe/tic-tac-toe/src/app/app.module.ts
@@ -3,15 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TTTComponent } from './ttt/ttt.component';
+import { TttComponent } from './ttt/ttt.component';
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
-    TTTComponent,
-    TTTComponent
+    TttComponent,
   ],
   imports: [
     BrowserModule,
