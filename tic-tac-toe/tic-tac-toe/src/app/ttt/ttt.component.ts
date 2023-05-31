31e1a5fb649c8565c0b5e0b34dae02b8cc5b08e7
Spectraza <96127639+Spectraza@users.noreply.github.com>
2023-05-30 19:31:58 -0500

    Game (#36)
    * started create a game
    
    * to add smth
    
    * game has been complited
    
    * one more time

--- tic-tac-toe/tic-tac-toe/src/app/ttt/ttt.component.ts
+++ tic-tac-toe/tic-tac-toe/src/app/ttt/ttt.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import './ttt'
+import {Component} from '@angular/core';
+import {Board, TicTacToe} from './ttt';
 
 @Component({
   selector: 'app-ttt',
@@ -8,10 +8,13 @@ import './ttt'
 })
 export class TTTComponent {
   ttt = new TicTacToe();
-
   icons = {
-    'X': 'clear',
-    'O': 'panorama_fish_eye',
-    null : 'menu'
+    X: 'clear',
+    O: 'panorama_fish_eye',
+    '': 'minimize',
+  };
+
+  playMove(i: number, j: number) {
+    this.ttt.playMove({row: i, col: j});
   }
 }
