549b47e1ae248346f583a93c34189511e87da590
Spectraza <savychvp@gmail.com>
2023-05-31 03:24:19 +0300

    game has been complited

--- tic-tac-toe/tic-tac-toe/src/app/app.module.ts
+++ tic-tac-toe/tic-tac-toe/src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TTTComponent } from './ttt/ttt.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
+    TTTComponent,
     TTTComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
